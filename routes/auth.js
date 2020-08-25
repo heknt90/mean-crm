41c2014ace1f214b81e14dc72c2a02427573a5e4
Nikita Efremov <heknt90@mail.ru>
2020-08-25 22:02:36 +0300

    Changed methods of auth requests

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const controller = require('../controllers/auth')
 
 // localhost:5000/api/auth/login
-router.get('/login', controller.login)
+router.post('/login', controller.login)
 // localhost:5000/api/auth/register
-router.get('/register', controller.register)
+router.post('/register', controller.register)
 
 
 module.exports = router
