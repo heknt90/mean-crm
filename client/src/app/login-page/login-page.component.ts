d353046e6bd23861b1cd5eaaa3f649f9ed9702c6
Nikita Efremov <heknt90@mail.ru>
2020-09-28 00:10:04 +0300

    форма авторизации

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  form: FormGroup
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(
+          null,
+          [Validators.required, Validators.email]
+        ),
+      password: new FormControl(
+          null,
+          [Validators.required, Validators.minLength(6)]
+        )
+    })
+  }
+
+  onSubmit() {
+    const email = this.form.controls.email.value
+    console.log(email)
   }
 
 }
