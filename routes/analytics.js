f55e34882a36bca4fb8e4963cb4afec3b2e40f54
Nikita Efremov <heknt90@mail.ru>
2020-10-08 17:42:46 +0300

    добавлена защита на страницы аналитики

--- routes/analytics.js
+++ routes/analytics.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/analytics')
+const passport = require('passport')
 
 // localhost:5000/api/analytics/analytics
-router.get('/analytics', controller.analytics)
+router.get('/analytics', passport.authenticate('jwt', {session: false}), controller.analytics)
 // localhost:5000/api/analytics/overview
-router.get('/overview', controller.overview)
+router.get('/overview', passport.authenticate('jwt', {session: false}), controller.overview)
 
 
 module.exports = router
