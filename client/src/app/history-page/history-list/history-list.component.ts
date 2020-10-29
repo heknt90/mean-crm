9ef041e5f7991b8b9c49befadb46fc0f4ca37606
Nikita Efremov <heknt90@mail.ru>
2020-10-29 22:12:09 +0300

    Созданы компоненты фильтра и списка заказов в истории

---
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Order } from 'src/app/shared/interfaces';
+
+@Component({
+  selector: 'app-history-list',
+  templateUrl: './history-list.component.html',
+  styleUrls: ['./history-list.component.scss']
+})
+export class HistoryListComponent implements OnInit {
+  @Input() orders: Order[]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { OrderService } from '../order-page/order.service';
+import { MaterialInstance, MaterialService } from '../shared/classes/material.service';
+import { Order } from '../shared/interfaces';
+import { OrdersService } from '../shared/services/orders.service';
+
+const STEP = 2
 
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.scss']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('tooltip') tooltipRef: ElementRef
+  tooltip: MaterialInstance
+  oSub: Subscription
+  isFilterVisible = false
+  orders: Order[] = []
+  
+  offset = 0
+  limit = STEP
+
+  loading = false
+  reloading = false
+  noMoreOrders = false
+
+  constructor(private ordersService: OrdersService) { }
+
+  loadMore() {
+    this.offset += STEP
+    this.loading = true
+    this.fetch()
+  }
 
   ngOnInit(): void {
+    this.reloading = true
+    this.fetch()
+  }
+
+  private fetch() {
+    const params = {
+      offset: this.offset,
+      limit: this.limit
+    }
+    this.oSub = this.ordersService.fetch(params).subscribe(
+      orders => {
+        this.orders = this.orders.concat(orders)
+        this.noMoreOrders = orders.length < STEP
+        this.loading = false
+        this.reloading = false
+      }
+    )
+  }
+
+  ngAfterViewInit() {
+    this.tooltip = MaterialService.initTooltip(this.tooltipRef)
+  }
+
+  ngOnDestroy() {
+    
+    this.tooltip.destroy()
+    this.oSub.unsubscribe()
   }
 
 }
