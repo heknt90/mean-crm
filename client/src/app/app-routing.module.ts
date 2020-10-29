68a67b0a21a3fa3dc30d6a15de48bfaf6bcfbddf
Nikita Efremov <heknt90@mail.ru>
2020-10-29 20:34:06 +0300

    добавлены компоненты заказов

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ import { CategoriesFormComponent } from './categories-page/categories-form/categ
 import { CategoriesPageComponent } from './categories-page/categories-page.component';
 import { HistoryPageComponent } from './history-page/history-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { OrderCategoriesComponent } from './order-page/order-categories/order-categories.component';
 import { OrderPageComponent } from './order-page/order-page.component';
+import { OrderPositionsComponent } from './order-page/order-positions/order-positions.component';
 import { OverviewPageComponent } from './overview-page/overview-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { AuthGuard } from './shared/classes/auth.guard';
@@ -30,7 +32,10 @@ const routes: Routes = [
       {path: 'overview', component: OverviewPageComponent},
       {path: 'analytics', component: AnalyticsPageComponent},
       {path: 'history', component: HistoryPageComponent},
-      {path: 'order', component: OrderPageComponent},
+      {path: 'order', component: OrderPageComponent, children: [
+        {path: '', component: OrderCategoriesComponent},
+        {path: ':id', component: OrderPositionsComponent}
+      ]},
       {path: 'categories', component: CategoriesPageComponent},
       {path: 'categories/new', component: CategoriesFormComponent},
       {path: 'categories/:id', component: CategoriesFormComponent},
