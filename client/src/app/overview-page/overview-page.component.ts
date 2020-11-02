2bc0314c38772ed9503a3e00437a100dd820b23a
Nikita Efremov <heknt90@mail.ru>
2020-11-02 15:39:04 +0300

    добавлена библиотека chart js. Логика страниц overview и analytics (Angular)

--- client/src/app/overview-page/overview-page.component.ts
+++ client/src/app/overview-page/overview-page.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MaterialInstance, MaterialService } from '../shared/classes/material.service';
+import { OverviewPage } from '../shared/interfaces';
+import { AnalyticsService } from '../shared/services/analytics.service';
 
 @Component({
   selector: 'app-overview-page',
   templateUrl: './overview-page.component.html',
   styleUrls: ['./overview-page.component.scss']
 })
-export class OverviewPageComponent implements OnInit {
+export class OverviewPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('tapTarget') tapTargetRef: ElementRef
+
+  tapTarget: MaterialInstance
+  data$: Observable<OverviewPage>
+
+  yesterday: Date = new Date()
+
+  constructor(private analyticsService: AnalyticsService) { }
 
   ngOnInit(): void {
+    this.data$ = this.analyticsService.getOverview()
+
+    this.yesterday.setDate(this.yesterday.getDate() - 1)
+  }
+
+  ngOnDestroy() {
+    this.tapTarget.destroy()
+
+  }
+
+  openInfo() {
+    this.tapTarget.open()
+  }
+
+  ngAfterViewInit() {
+    this.tapTarget = MaterialService.initTapTarget(this.tapTargetRef)
   }
 
 }
